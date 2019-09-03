e66f2d10d2a8d1a4fe940ca0c78d40f94f7fedb1
william kämäräinen <william.kamarainen@gmail.com>
2019-09-03 11:23:43 +0200

    test

---
+++ auktion/src/components/auktion/AuktionContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class AuktionContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
+
+export default AuktionContainer
+
