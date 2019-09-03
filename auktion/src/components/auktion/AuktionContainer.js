81ccb8acdbc9f67fb33e883c7ea4f207ef2701c5
william kämäräinen <william.kamarainen@gmail.com>
2019-09-03 13:21:29 +0200

    commit

--- auktion/src/components/auktion/AuktionContainer.js
+++ auktion/src/components/auktion/AuktionContainer.js
@@ -4,11 +4,7 @@ export class AuktionContainer extends Component {
     render() {
         return (
             <div>
-<<<<<<< Updated upstream
-                <h1>afögjaöflkglsfjglsk</h1>
-=======
                 <h1>sdfsdf</h1>
->>>>>>> Stashed changes
             </div>
         )
     }
