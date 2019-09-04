9c38422c97c85b831e306ec6a329786ea5cbde24
WilliamK97 <william.kamarainen@gmail.com>
2019-09-04 10:58:07 +0200

    små fix på auktions korten

--- auktion/src/components/auktion/AuktionList.js
+++ auktion/src/components/auktion/AuktionList.js
@@ -15,7 +15,13 @@ export default class AuktionList extends React.Component{
                                 <div className="card-title orange-text">{a.Titel}</div>
                                 <div class="card-content">
                                     <p>{a.Beskrivning}</p>
+                                    <hr/>
+                                    <p>{a.Utropspris} kr</p>
+                                    <hr/>
+                                    <p>Slutdatum: {a.SlutDatum.slice(5, -9)}</p>
+                                    
                                 </div>
+                                {/* fixa route här till annonsen på en ny sida */}
                                 <div class="card-action padding">
                                     <a href="#">Till annons</a>
                                 </div>
@@ -28,7 +34,7 @@ export default class AuktionList extends React.Component{
         });
 
         return(<div>
-            
+
                 <h1> Alla auktioner </h1>
                 <div> {auktion} </div>
             
