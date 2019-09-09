fcacac85aad20ca8ae61c05f6be1a48e92c093a3
Michael Lyth <mickelyth@outlook.com>
2019-09-09 13:36:20 +0200

    Lagt till nytt bud formulär och post

--- auktion/src/components/auktion/Bud.js
+++ auktion/src/components/auktion/Bud.js
@@ -1,12 +1,71 @@
 import React from 'react'
-import AuktionContainer from './AuktionContainer'
 
-export default function Tables(props){
-    const budArray = props.bud !== undefined ? (props.bud.map(bud => {
+export default class Bud extends React.Component{
+    state = {
+        bud: [],
+        summa: null,
+        auktionID: null,
+        budgivare: null
+    }
+    handleChange = (e) => {
+        this.setState({
+          [e.target.id]: e.target.value
+        });
+      }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+        const aktionsid = this.props.auktion.AuktionID;
+
+        const url = 'http://nackowskis.azurewebsites.net/api/Bud/2130/'
+        const url2 = `http://nackowskis.azurewebsites.net/api/Bud/2130/${aktionsid}`
+        const bud = e.target.summa.value;
+
+        fetch(url2)
+        .then(response => response.json())
+        .then(json => 
+            this.setState({
+                bud: json
+        }));
+
+        // function checkSum(sum) {
+        //     return sum < bud;
+        // }
+
+        // const hogstaBud = this.props.bud.filter(checkSum);
+
+        const hogstaBud = this.props.bud.filter(b => {
+            return e.target.summa.value < b.Summa+1
+        })
+
+        if (hogstaBud.length !== 0) {
+            return alert ('Hej');
+        }
+        else {
+            const nyttBud = {"Summa": e.target.summa.value,
+                             "Budgivare": e.target.budgivare.value,
+                             "AuktionID": aktionsid}
+
+            fetch(url,{
+                method: 'POST',
+                body: JSON.stringify(nyttBud),
+                headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+                }
+                }).then(function (data) {
+                console.log('Request success: ', 'posten skapad');
+               })       
+        }
+    }
+    render(){
+    const budArray = this.props.bud !== undefined ? (this.props.bud.map(bud => {
         return(
             <span key={bud.BudID}>
                 <li>{bud.Budgivare}</li>
                 <li>{bud.Summa}</li>
+                <li>{bud.AuktionID}</li>
             </span>
         )
     })) : (
@@ -17,10 +76,22 @@ export default function Tables(props){
 
     return(
         <div>
-            <h1>{props.auktion.Titel}</h1>
-            <p>{props.auktion.Beskrivning}</p>
-            <p>{props.auktion.SlutDatum}</p>
+            <h1>{this.props.auktion.Titel}</h1>
+            <p>{this.props.auktion.Beskrivning}</p>
+            <p>{this.props.auktion.SlutDatum}</p>
+            <p>{this.props.auktion.AuktionID}</p>
             <ul>{budArray}</ul>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <label htmlFor="summa">Summa: </label>
+                <input type="text" id="summa" name="summa" onChange={this.handleChange}/>
+                <label htmlFor="budgivare">Budgivare: </label>
+                <input type="text" id="budgivare" onChange={this.handleChange}/>
+                <button>Lägg bud</button>
+                </form>
+            </div>
         </div>
     )
 }
+}
+
